63e85ce9775aa9d58a4181fc927ac1c5574f5ab9
Mikael <mikael.thorman@gmail.com>
2024-04-22 17:05:31 +0200

    updated bat files to execute in the work folder

---
+++ scripts/open_vs_code.bat
@@ -0,0 +1,4 @@
+echo off
+
+REM Open VS Code in the code folder, obs run powershell not cmd otherwise the folder will default to Windows in VS code
+code "\\wsl$\docker-desktop-data\data\docker\volumes\local-ai-folder\_data"
---
+++ test-files/run.bat
@@ -0,0 +1,4 @@
+echo off
+
+REM this is a script runs the deployed docker instance
+docker restart local-crew-ai
