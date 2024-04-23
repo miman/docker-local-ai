faeaba4151665e8bffbf2ed6d7775163b57bd036
Mikael Thorman <mikael.thorman@gmail.com>
2024-04-23 15:26:10 +0200

    Update install.bat
    gpus all fix

--- install.bat
+++ install.bat
@@ -4,7 +4,7 @@ REM The docker containers needs a common network to be able to communicate with 
 docker network create local-ai-network
 
 REM Install the ollama Docker container
-docker run -d --gpus=all --network local-ai-network --restart always -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+docker run -d --gpus all --network local-ai-network --restart always -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
 
 REM Install the ollama WebUI Docker container
 docker run -d -p 4512:8080 --network local-ai-network --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
