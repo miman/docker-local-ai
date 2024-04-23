47c5097c9b5719babe2f2eaaff954e70b3ef5bdb
Mikael Thorman <mikael.thorman@gmail.com>
2024-04-23 08:05:58 +0200

    Update cleanup.bat
    not removing infra

--- scripts/cleanup.bat
+++ scripts/cleanup.bat
@@ -1,20 +1,14 @@
 echo off
 
-REM This scripts removes all created docker containers, networks & volumes
+REM This scripts removes all created docker containers but NOT the networks & volumes
 
 REM OBS, this file will remove all volumes as well, if you want to keep these remove the -v flags in the rows below
 
 REM Uninstall the local-crew-ai Docker container
-docker rm -f -v local-crew-ai
+docker rm -f local-crew-ai
 
 REM Uninstall the ollama Docker container
-docker rm -f -v ollama
+docker rm -f ollama
 
 REM Uninstall the ollama WebUI Docker container
-docker rm -f -v open-webui
-
-REM Remove the volume for the Used by CrewAI Docker container
-docker volume rm local-ai-folder
-
-REM The docker containers needs a common network to be able to communicate with each other
-docker network rm -f local-ai-network
+docker rm -f open-webui
