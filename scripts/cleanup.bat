2eb50969e6b2d062cd8774753c15bda52249dac6
Mikael Thorman <mikael.thorman@gmail.com>
2024-04-24 10:39:58 +0200

    Update cleanup.bat
    also removing the images

--- scripts/cleanup.bat
+++ scripts/cleanup.bat
@@ -6,9 +6,16 @@ REM OBS, this file will remove all volumes as well, if you want to keep these re
 
 REM Uninstall the local-crew-ai Docker container
 docker rm -f local-crew-ai
+docker rmi local-crew-ai:latest
 
 REM Uninstall the ollama Docker container
 docker rm -f ollama
+docker rmi ollama/ollama:latest
 
 REM Uninstall the ollama WebUI Docker container
 docker rm -f open-webui
+docker rmi ghcr.io/open-webui/open-webui:main
+
+REM Uninstall the Flowise Docker container
+docker rm -f flowise
+docker rmi flowise:latest
