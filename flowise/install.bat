1fd98542364dd689cd19acf6e29ad6485c002a50
Mikael Thorman <mikael.thorman@gmail.com>
2024-04-24 10:05:37 +0200

    Update install.bat
    flowise install

--- flowise/install.bat
+++ flowise/install.bat
@@ -3,5 +3,18 @@ echo off
 REM Installing Flowise into a Docker container
 
 REM Fetch the latest version of Flowise from GitHub
-git pull https://github.com/FlowiseAI/Flowise.git
+git clone https://github.com/FlowiseAI/Flowise.git
 
+REM Build the retrieved repository
+cd Flowise
+
+npx --yes pnpm install
+
+REM Create & install the Docker image for Flowise & expose on port 4511
+docker build --no-cache -t flowise .
+
+docker run -d --name flowise -p 4511:3000 flowise
+
+echo Flowise has been installed and is accessible on http://localhost:4511
+
+cd ..
