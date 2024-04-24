017a9801a585b59cef39feac5efece8491750565
Mikael Thorman <mikael.thorman@gmail.com>
2024-04-24 12:25:30 +0200

    Update install.bat
    writing state to console

--- crewai/install.bat
+++ crewai/install.bat
@@ -4,6 +4,7 @@ REM Create a volume for the Used by CrewAI Docker container
 docker volume create local-ai-folder
 
 REM Copy the Python code to the CrewAI Docker volume
+echo Copying the CrewAI Python code to the Docker volume...
 copy code\*.* \\wsl$\docker-desktop-data\data\docker\volumes\local-ai-folder\_data
 move \\wsl$\docker-desktop-data\data\docker\volumes\local-ai-folder\_data\.env.template \\wsl$\docker-desktop-data\data\docker\volumes\local-ai-folder\_data\.env
 
@@ -11,6 +12,7 @@ mkdir \\wsl$\docker-desktop-data\data\docker\volumes\local-ai-folder\_data\outpu
 copy code\output\*.* \\wsl$\docker-desktop-data\data\docker\volumes\local-ai-folder\_data\output
 
 REM Deploy the CrewAI Docker container
+echo Creating and deploying CrewAI Docker container...
 docker-compose build
 
 docker-compose up 
