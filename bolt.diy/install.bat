fa4c0a86850685805b5fa60f4ce69cd519700b16
Mikael <mikael.thorman@gmail.com>
2025-02-06 15:36:30 +0100

    cloning bolt.diy code base instead of ready zip file

--- bolt.diy/install.bat
+++ bolt.diy/install.bat
@@ -3,16 +3,16 @@ echo off
 REM Installing Bolt.diy into a Docker container
 
 REM Fetch the latest version of Bolt.diy from GitHub
-curl -L https://github.com/stackblitz-labs/bolt.diy/archive/refs/tags/v0.0.6.zip -o v0.0.6.zip
-
 if exist "Bolt.diy" (
   echo Bolt.diy folder exists -> just pulling the latest changes
   cd Bolt.diy
   git pull
   cd ..
 ) else (
-  echo Bolt.diy folder does not exist -> Cloning repo from GtiHub
+  echo Bolt.diy folder does not exist -> Cloning repo from GitHub
   git clone -b v0.0.6 https://github.com/stackblitz-labs/bolt.diy.git Bolt.diy
+  copy Bolt.diy\.env.example Bolt.diy\.env.local
+  call powershell -Command "(Get-Content Bolt.diy\.env.local) -replace 'OLLAMA_API_BASE_URL=.*', 'OLLAMA_API_BASE_URL=http://host.docker.internal:11434' | Set-Content Bolt.diy\.env.local"
 )
 
 REM Build the retrieved repository
@@ -21,10 +21,7 @@ cd Bolt.diy
 REM Using npm script:
 call npm run dockerbuild
 
-REM OR using direct Docker command:
-@REM docker build . --target bolt-ai-development
-
-@REM REM Create & install the Docker image for Bolt.diy & expose on port 4511
+REM Create & install the Docker image for Bolt.diy & expose on port 4511
 echo Creating and deploying Bolt.diy Docker container...
 
 call docker compose --profile development up
