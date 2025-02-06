c614a5f9c7e77f9393903df0069e30e270653c74
Mikael Thorman <mikael.thorman@gmail.com>
2025-02-06 15:29:45 +0100

    Update install.bat
    ensuring the latest n8n is used

--- n8n/install.bat
+++ n8n/install.bat
@@ -1,16 +1,27 @@
 echo off
 
 REM The docker containers needs a common network to be able to communicate with each other
-docker network create local-ai-network
+docker network ls | findstr /r /c:"local-ai-network" >nul
+IF ERRORLEVEL 1 (
+    docker network create local-ai-network
+) ELSE (
+    echo The network local-ai-network already exists.
+)
 
 REM Create a volume for the Used by Swarm Docker container
-docker volume create n8n_data
-
-REM Installing N8N into a Docker container
-echo Creating Docker container...
-docker-compose build
+docker volume ls | findstr /r /c:"n8n_data" >nul
+IF ERRORLEVEL 1 (
+    docker volume create n8n_data
+) ELSE (
+    echo The volume n8n_data already exists.
+)
 
 REM Start in detached mode
 echo Deploying Docker container...
-docker-compose up -d
+docker-compose down
+docker-compose pull
+docker-compose up -d --force-recreate --build
 
+REM Remove dangling images
+echo Removing dangling Docker images...
+docker image prune -f
