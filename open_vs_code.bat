e141ab626c470b449b8770befbc061ce28ab5195
Mikael <mikael.thorman@gmail.com>
2025-02-07 16:11:30 +0100

    removed setting local folder while it creates a lot of VS code folders in the current folder

--- open_vs_code.bat
+++ open_vs_code.bat
@@ -3,9 +3,9 @@ echo off
 set CURRENT_PATH=%CD%
 REM Open VS Code in the code folder, obs run powershell not cmd otherwise the folder will default to Windows in VS code
 if exist "\\wsl$\docker-desktop\mnt\docker-desktop-disk" (
-   code --user-data-dir "%CURRENT_PATH%" "\\wsl$\docker-desktop\mnt\docker-desktop-disk\data\docker\volumes\local-ai-folder\_data"
+   code "\\wsl$\docker-desktop\mnt\docker-desktop-disk\data\docker\volumes\local-ai-folder\_data"
 ) else (
    if exist "\\wsl.localhost\docker-desktop-data\data\docker\volumes" (
-      code --user-data-dir "%CURRENT_PATH%" "\\wsl.localhost\docker-desktop-data\data\docker\volumes\local-ai-folder\_data"
+      code "\\wsl.localhost\docker-desktop-data\data\docker\volumes\local-ai-folder\_data"
    )
 )
