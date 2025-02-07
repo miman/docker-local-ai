134468edc9172893248577fd0a675d66f4571c3f
Mikael <mikael.thorman@gmail.com>
2025-02-07 16:04:46 +0100

    ensuring AnythinLLm is always updated at install

--- AnythingLLM/install.bat
+++ AnythingLLM/install.bat
@@ -5,5 +5,11 @@ SET STORAGE_LOCATION="C:\Development\docker-storage\anythingllm"
 if not exist "%STORAGE_LOCATION%" mkdir "%STORAGE_LOCATION%"
 if not exist "%STORAGE_LOCATION%\.env" echo DISABLE_TELEMETRY='true' >> "%STORAGE_LOCATION%\.env"
 
-REM Install the AnythingLLM Docker container
-docker run -d --gpus all --network local-ai-network --restart always -p 4513:3001 --cap-add SYS_ADMIN -v "%STORAGE_LOCATION%:/app/server/storage" -v "%STORAGE_LOCATION%\.env:/app/server/.env" -e STORAGE_DIR="/app/server/storage" --name anythingllm mintplexlabs/anythingllm
+echo "Removing the old AnythingLLM container..."
+docker rm -f anythingllm
+
+echo "Fetching the latest version of the AnythingLLM image..."
+docker pull mintplexlabs/anythingllm
+
+echo "Install the AnythingLLM Docker container..."
+docker run -d --gpus all --network local-ai-network -p 4513:3001 --cap-add SYS_ADMIN -v "%STORAGE_LOCATION%:/app/server/storage" -v "%STORAGE_LOCATION%\.env:/app/server/.env" -e STORAGE_DIR="/app/server/storage" --name anythingllm mintplexlabs/anythingllm
