b343ae3cdace23e1be63ceb5a4f3ed3b1e23dc85
Mikael <mikael.thorman@gmail.com>
2025-02-09 01:22:18 +0100

    removed duplicate swarm files

--- swarm/install.bat
+++ swarm/install.bat
@@ -5,11 +5,11 @@ docker volume create swarm-ai-folder
 
 REM Copy the Python code to the Swarm Docker volume
 echo Copying the Swarm Python code to the Docker volume...
-copy code\*.* \\wsl$\docker-desktop-data\data\docker\volumes\swarm-ai-folder\_data
+copy TestSwarmPrj\demo_code\*.* \\wsl$\docker-desktop-data\data\docker\volumes\swarm-ai-folder\_data
 move \\wsl$\docker-desktop-data\data\docker\volumes\swarm-ai-folder\_data\.env.template \\wsl$\docker-desktop-data\data\docker\volumes\swarm-ai-folder\_data\.env
 
 mkdir \\wsl$\docker-desktop-data\data\docker\volumes\swarm-ai-folder\_data\output
-copy code\output\*.* \\wsl$\docker-desktop-data\data\docker\volumes\swarm-ai-folder\_data\output
+copy TestSwarmPrj\demo_code\output\*.* \\wsl$\docker-desktop-data\data\docker\volumes\swarm-ai-folder\_data\output
 
 REM Deploy the Swarm Docker container
 echo Creating and deploying Swarm Docker container...
