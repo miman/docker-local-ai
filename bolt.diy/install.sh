097d2d6f91b3539e5faf2603dae6527f4f8f0eaa
Mikael Thorman <mikael.thorman@volvo.com>
2025-02-17 11:17:02 +0100

    mac fix in bolt install script

--- bolt.diy/install.sh
+++ bolt.diy/install.sh
@@ -3,17 +3,18 @@
 
 set -e
 
-echo "Installing Bolt.diy into a Docker container..."
+BOLT_VER="v0.0.6"
+echo "Installing Bolt.diy '$BOLT_VER' into a Docker container..."
 
 # Check if the Bolt.diy directory exists. If so, pull the latest changes; if not, clone the repo.
 if [ -d "Bolt.diy" ]; then
   echo "Bolt.diy folder exists -> just pulling the latest changes"
   cd Bolt.diy
-  git pull
+  git pull origin $BOLT_VER
   cd ..
 else
   echo "Bolt.diy folder does not exist -> Cloning repo from GitHub"
-  git clone -b v0.0.6 https://github.com/stackblitz-labs/bolt.diy.git Bolt.diy
+  git clone -b $BOLT_VER https://github.com/stackblitz-labs/bolt.diy.git Bolt.diy
   cp Bolt.diy/.env.example Bolt.diy/.env.local
 
   # Replace OLLAMA_API_BASE_URL value using sed.
