6eaebe170ceb3f1e9b17835171439469e1378d47
Mikael <mikael.thorman@gmail.com>
2025-02-17 07:10:25 +0100

    Added nvidia question to sh install script

--- ollama/install.sh
+++ ollama/install.sh
@@ -9,16 +9,27 @@ else
 fi
 
 # Prompt for Ollama UI installation
+read -p "Do you have an Nvidia GPU you want to use with Ollama (y/N)?  " answerNvidia
+echo "Deploying Docker container..."
+if [[ "$answerNvidia" =~ [Yy]$ ]]; then
+  echo "Using Nvidia card in Ollama"
+  COMPOSE_PART=-f docker-compose.yaml -f docker-compose-nvidia.yaml
+else
+  COMPOSE_PART=
+fi
+# echo "COMPOSE_PART = $COMPOSE_PART"
+
+# Prompt for Ollama UI installation
 read -p "Do you want to install the Ollama UI (y/N)? " answer
 echo "Deploying Docker container..."
 if [[ "$answer" =~ [Yy]$ ]]; then
   docker-compose down
   docker-compose pull
-  docker-compose up -d --force-recreate --build
+  docker-compose $COMPOSE_PART up -d --force-recreate --build
 else
   docker-compose down
   docker-compose pull ollama-container
-  docker-compose up -d --force-recreate --build ollama-container
+  docker-compose $COMPOSE_PART up -d --force-recreate --build ollama-container
 fi
 
 echo "Ollama has been installed and is accessible on http://localhost:4512"
