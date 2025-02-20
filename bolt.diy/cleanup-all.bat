1b4f929f35e08835e328100a2b8e7eab07ccf465
Alex <alex.naga@hotmail.com>
2025-02-20 14:45:38 +0100

    chore: clean up

--- bolt.diy/cleanup-all.bat
+++
@@ -1,8 +0,0 @@
-echo off
-
-REM This scripts removes all created docker containers, networks & volumes
-
-REM OBS, this file will remove all volumes as well, if you want to keep these remove the -v flags in the rows below or run the cleanup.bat script
-
-REM Uninstall the bolt.diy Docker container
-docker-compose down -v --rmi all
