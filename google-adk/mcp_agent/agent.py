271991133622f92307641805ea54ec71ac8fc4a0
Mikael <mikael.thorman@gmail.com>
2025-05-02 12:29:04 +0200

    Refactor mcp_tools.py to streamline tool fetching and remove unused code

--- google-adk/mcp_agent/agent.py
+++ google-adk/mcp_agent/agent.py
@@ -1,30 +1,6 @@
 from google.adk.agents import Agent
+from mcp_agent.tools.mcp_tools import fetch_tools
 from weather_agent.models import tool_calling_model
-from weather_agent.tools import get_weather
-from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset, StdioServerParameters
-import os
-
-# Fetch current folder path
-current_folder_path = os.path.abspath('.')
-print(f"Current folder path: {current_folder_path}")
-
-
-async def fetch_tools():
-    """Gets tools from MCP Server & local files."""
-    tools, exit_stack = await MCPToolset.from_server(
-        # Use StdioServerParameters for local process communication
-        connection_params=StdioServerParameters(
-            command='npx',  # Command to run the server
-            args=["-y",    # Arguments for the command
-                  "@modelcontextprotocol/server-filesystem",
-                  current_folder_path],
-        )
-    )
-
-    # add weather service
-    tools.append(get_weather)
-
-    return tools, exit_stack
 
 
 async def create_agent():
